0a798e1d866049aedc75678185ad9e519c9aadbb
Shaun Chowthee <sn.chowthee@gmail.com>
2025-12-26 04:03:39 +0100

    feat: read action/transform and text from CLI args

--- src/dtcli/cli.py
+++ src/dtcli/cli.py
@@ -1,3 +1,7 @@
-print("initializing program")
-name = input("Please enter your name")
-print(f"Oh hello, {name}!")
+import sys
+
+if len(sys.argv) < 3:
+    print("need an action AND a text")
+    print('Usage: dt <action> "<text>"')
+    sys.exit(2)
+print(f"Action received: {sys.argv[1]}\nText received: {sys.argv[2]}")
